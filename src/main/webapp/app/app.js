2f7cc963e2ed737fafd5d07ff777ba9aca86b732
Guramritpal Singh <guramritpal.singh@appdirect.com>
2017-04-10 16:38:32 +0530

    activities routing

--- src/main/webapp/app/app.js
+++ src/main/webapp/app/app.js
@@ -1,21 +1,21 @@
 'use strict';
 
 (function() {
-   var app = window.angular.module("tracking-app", ['controllers', 'services', 'ngResource','ngRoute']);
+    var app = window.angular.module("tracking-app", ['controllers', 'services', 'ngResource','ngRoute']);
 
-   app.config(function($routeProvider) {
-      $routeProvider
-          .when("/", {
-             templateUrl:"app/templates/home.html"
-          })
-          .when('/activities', {
-          templateUrl:'app/templates/activities.html',
-          controller: "activityController"
-      })
-   });
+    app.config(function($routeProvider) {
+        $routeProvider
+            .when("/", {
+                templateUrl:"app/templates/home.html"
+            })
+            .when('/activities', {
+                templateUrl:'app/templates/activities.html',
+                controller: "activityController"
+            })
+    });
     /*app.config(['$resourceProvider', function($resourceProvider) {
-        // Don't strip trailing slashes from calculated URLs
-        $resourceProvider.defaults.stripTrailingSlashes = false;
-    }]);*/
+     // Don't strip trailing slashes from calculated URLs
+     $resourceProvider.defaults.stripTrailingSlashes = false;
+     }]);*/
 })();
 
